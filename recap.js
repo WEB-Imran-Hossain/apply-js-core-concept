9b16cfc7bd2a0657feb8173a48e8930fb9a7a24e
Imran <webimran2021@gmail.com>
2023-08-04 23:08:49 +0600

    recap previous lession

---
+++ recap.js
@@ -0,0 +1,48 @@
+// 1. Variable
+var myName = "Khan";
+
+// math operation: +, -, *, /
+// shorthand: +=, ++, --, *=, /=
+// ++, --
+
+// 2. Array
+var friends = ['abul', 'babul', 'kabul', 'dabul', 'ebul'];
+// Array Index
+ var thisrdfriend = friends[2];
+ friends[3] = 'dambul';
+// Array position
+
+// conditional
+// >, <, >=, <=, ==, ===, !=, !==
+if (friends.length < 2) {
+    console.log('fokir tor kono friend nai');
+}
+
+else{
+
+}
+
+// while loop  and for loop
+var number = 0;
+while (number <= 6) {
+    console.log(number);
+    number++;
+}
+
+for(var i = 0; i <= 10; i++){
+
+}
+
+// function
+function isMoonUp(time){
+    if (time > 7) {
+        return true;
+    }
+}
+
+// Object
+var jantus = {
+    height: 60,
+    romanticism: "heavvy",
+    bappErtaka: "valo"
+}
