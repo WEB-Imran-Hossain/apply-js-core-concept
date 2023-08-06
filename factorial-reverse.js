0dad2ce1af06235bc6b6c8fcea89ae9e1784fe2a
Imran <webimran2021@gmail.com>
2023-08-06 19:41:20 +0600

    added factoriaL

---
+++ factorial-reverse.js
@@ -0,0 +1,22 @@
+// reverse factorial
+function factorial(number) {
+    for(let i = number; i >= 1; i--){
+        console.log(i);
+    }
+}
+
+factorial(7);
+
+
+// multiply
+function factorial(number) {
+    let result = 1;
+    for(let i = number; i >= 1; i--){
+        result = result * i;
+        console.log(i);
+    }
+    return result;
+}
+const number = 5;
+const fact = factorial(number);
+console.log('factorial of:', number, fact);
